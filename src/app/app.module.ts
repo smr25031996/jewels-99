d1eb9fff6e5bdec555655962f15d5b12e11f5571
Shubham Rajput <rajputshubham615@gmail.com>
2023-10-30 17:04:19 +0530

    importing my website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
